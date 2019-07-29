22a74336cf5bd89f10c03338fd72167c831589f3
Rzhevsky-forever <egorbarisovi4@gmail.com>
2019-07-30 01:36:55 +0500

    Задание 3

--- ex-3.js
+++ ex-3.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var star = '*';
 star = star++;
 console.log(star); // NAN
@@ -10,3 +12,23 @@ var star = '*';
 star += star;
 console.log(star); // **
 
+// Задача 1
+var positions = [
+    'Отвертка ультразвуковая WHO-D',
+    'Ховерборд Mattel 2016',
+    'Нейтрализатор FLASH black edition',
+    'Меч световой FORCE (синий луч)',
+    'Машина времени DeLorean',
+    'Репликатор домашний STAR-94',
+    'Лингвенсор 000-17',
+    'Целеуказатель электронный WAY-Y'
+  ]
+
+var listLength = positions.length;
+console.log('Список наименований');
+var count = 1;
+for (var i = 0; i < listLength; i++)
+{
+    console.log(count + ' ' + positions[i]);
+    count++;
+}
