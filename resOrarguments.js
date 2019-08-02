4d185f02cedad50c5a0e1ff772a615ddcb30ac4b
egorbarisovi4 <egorbarisovi4@gmail.com>
2019-08-02 20:50:42 +0500

    resrExample

---
+++ resOrarguments.js
@@ -0,0 +1,15 @@
+function summ(...rest)
+{
+    console.log(rest);
+    console.log(arguments);
+
+    var result = 0;
+    for(var item of rest)
+    {
+        result += item;
+    }
+
+    return result;
+}
+
+console.log(summ(1, 2, 3));
